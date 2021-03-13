19e6e4b901975c584021c321a301388e0365d356
xushengjun <xsj_ff@163.com>
2021-03-14 01:39:18 +0800

    week8 day2 homework2 补充eurake服务

--- Week_08/day2/homework2/hmily-parent-demo/eurake-server/src/main/java/org/example/EurakeServerApplication.java
+++ Week_08/day2/homework2/hmily-parent-demo/eurake-server/src/main/java/org/example/EurakeServerApplication.java
@@ -1,2 +1,13 @@
-package org.example;public class EurakeServerApplication {
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurakeServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurakeServerApplication.class,args);
+    }
 }
